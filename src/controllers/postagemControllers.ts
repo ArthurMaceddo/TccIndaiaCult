83641eec04e1f5c9b5d185a263a2138254536bd7
ArthurMaceddo <arthurmaceddo@gmail.com>
2024-05-10 15:06:33 -0300

    Adicionando o que faltava na Postagem e corrigindo req.params no deleteUser

--- src/controllers/postagemControllers.ts
+++ src/controllers/postagemControllers.ts
@@ -1,42 +1,49 @@
-import { Request, Response } from 'express';
-import { authenticate } from '../middleware/authMiddleware';
-import Postagem, { IPostagem } from '../models/Postagem';
+import { Request, Response } from "express";
+import { authenticate } from "../middleware/authMiddleware";
+import Postagem, { IPostagem } from "../models/Postagem";
 
 const createPostagem = async (req: Request, res: Response) => {
   try {
-    const { title, description } = req.body;
+    const { id, title, category, description, image, artista, artistaImage } =
+      req.body;
 
     if (!title || !description) {
       return res
         .status(400)
-        .json({ message: 'Por favor, forneça todos os campos necessários' });
+        .json({ message: "Por favor, forneça todos os campos necessários" });
     }
 
     const novaPostagem: IPostagem = new Postagem({
+      id,
       title,
+      category,
       description,
+      image,
+      artista,
+      artistaImage,
     });
 
     await novaPostagem.save(); // Adicionei o await aqui para aguardar a conclusão da operação de salvamento
 
-    res
-      .status(201)
-      .json({
-        message: 'Postagem criada com sucesso.',
-        postagem: novaPostagem,
-      });
+    res.status(201).json({
+      message: "Postagem criada com sucesso.",
+      postagem: novaPostagem,
+    });
   } catch (error) {
-    console.error('Erro ao criar postagem:', error);
-    res.status(500).json({ message: 'Ocorreu um erro ao criar a postagem.' });
+    console.error("Erro ao criar postagem:", error);
+    res.status(500).json({ message: "Ocorreu um erro ao criar a postagem." });
   }
 };
 
 const getPostagem = async (req: Request, res: Response) => {
   const { postagemId } = req.params;
-  const postagem = await Postagem.findById(postagemId, 'tittle description');
+  const postagem = await Postagem.findById(
+    postagemId,
+    "tittle description category artista"
+  );
 
   if (!postagem) {
-    res.status(400);
+    return res.status(404).json({ message: "Postagem não encontrada" });
   }
 
   res.status(200).json(postagem);
@@ -44,10 +51,13 @@ const getPostagem = async (req: Request, res: Response) => {
 
 const listPostagem = async (req: Request, res: Response) => {
   try {
-    const postagens = await Postagem.find({}, 'title description');
+    const postagens = await Postagem.find(
+      {},
+      "tittle description category artista"
+    );
     res.status(200).json(postagens);
   } catch (Error) {
-    res.status(500).json('Ocorreu um erro na listagem de postagens.');
+    res.status(500).json("Ocorreu um erro na listagem de postagens.");
   }
 };
 
@@ -57,17 +67,15 @@ const deletePostagem = async (req: Request, res: Response) => {
     const postagem = await Postagem.findByIdAndDelete(postagemId);
 
     if (!postagem) {
-      return res.status(404).json({ message: 'Postagem não encontrada' });
+      return res.status(404).json({ message: "Postagem não encontrada" });
     }
 
-    res.status(200).json({ message: 'Postagem deletada com sucesso' });
+    res.status(200).json({ message: "Postagem deletada com sucesso" });
   } catch (Error) {
-    res
-      .status(500)
-      .json({
-        message:
-          'Ocorreu um erro enquanto se deleveta a postagem, tente novamente.',
-      });
+    res.status(500).json({
+      message:
+        "Ocorreu um erro enquanto se deleveta a postagem, tente novamente.",
+    });
   }
 };
 
