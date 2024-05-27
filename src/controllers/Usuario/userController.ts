d1b38b229f3d54d190edd9d3a47b1355e25647fe
ArthurMaceddo <arthurmaceddo@gmail.com>
2024-05-26 21:38:51 -0300

    Adicionando update para Usuario

--- src/controllers/Usuario/userController.ts
+++ src/controllers/Usuario/userController.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from 'express';
-import Usuario from '../../models/Usuario';
-import { UsuarioBasicInfo } from '../../types/usuarioTypes';
+import { Request, Response } from "express";
+import Usuario from "../../models/Usuario";
+import { UsuarioBasicInfo } from "../../types/usuarioTypes";
 
 const createUsuario = async (req: Request, res: Response) => {
   try {
@@ -9,12 +9,12 @@ const createUsuario = async (req: Request, res: Response) => {
     if (!nome || !email || !senha || !imagem) {
       return res
         .status(400)
-        .json({ message: 'Por favor, forneça todos os campos necessários.' });
+        .json({ message: "Por favor, forneça todos os campos necessários." });
     }
 
     const existingUsuario = await Usuario.findOne({ email });
     if (existingUsuario) {
-      return res.status(400).json({ message: 'Este email já está em uso.' });
+      return res.status(400).json({ message: "Este email já está em uso." });
     }
 
     const newUsuario = new Usuario({
@@ -28,16 +28,16 @@ const createUsuario = async (req: Request, res: Response) => {
 
     res
       .status(201)
-      .json({ message: 'Usuario criado com sucesso.', user: newUsuario });
+      .json({ message: "Usuario criado com sucesso.", user: newUsuario });
   } catch (error) {
-    console.error('Erro ao criar Usuario:', error);
-    res.status(500).json({ message: 'Ocorreu um erro ao criar o Usuario.' });
+    console.error("Erro ao criar Usuario:", error);
+    res.status(500).json({ message: "Ocorreu um erro ao criar o Usuario." });
   }
 };
 
 const getUsuario = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const usuario = await Usuario.findById(id, 'nome email');
+  const usuario = await Usuario.findById(id, "nome email");
 
   if (!usuario) {
     res.status(400);
@@ -48,10 +48,10 @@ const getUsuario = async (req: Request, res: Response) => {
 
 const listUsuario = async (req: Request, res: Response) => {
   try {
-    const usuario = await Usuario.find({}, 'nome email');
+    const usuario = await Usuario.find({}, "nome email");
     res.status(200).json(usuario);
   } catch (Error) {
-    res.status(500).json(' Ocorreu um erro na listagem de usuario');
+    res.status(500).json(" Ocorreu um erro na listagem de usuario");
   }
 };
 
@@ -61,14 +61,32 @@ const deleteUsuario = async (req: Request, res: Response) => {
     const usuario = await Usuario.findByIdAndDelete(usuarioId);
 
     if (!usuario) {
-      return res.status(404).json({ message: 'Usuario not found.' });
+      return res.status(404).json({ message: "Usuario not found." });
     }
 
-    res.status(200).json({ message: 'Usuario deletado com sucesso' });
+    res.status(200).json({ message: "Usuario deletado com sucesso" });
   } catch (Error) {
     res
       .status(500)
-      .json({ message: 'Ocorreu um erro enquanto se deletava o Usuario' });
+      .json({ message: "Ocorreu um erro enquanto se deletava o Usuario" });
   }
 };
-export { createUsuario, getUsuario, listUsuario, deleteUsuario };
+
+const updateUsuario = async (req: Request, res: Response) => {
+  const { usuarioId } = req.params;
+  const { usuario } = req.body;
+
+  if (!usuarioId) {
+    res.status(500).json({ message: "Não foi possível encontrar o Usuario" });
+  }
+
+  try {
+    await Usuario.findByIdAndUpdate(usuarioId, usuario);
+    res.status(200).json({ message: " Usuario atualizado com sucesso" });
+  } catch (Error) {
+    res
+      .status(500)
+      .json({ message: "Ocorreu um erro enquanto atualizava o Usuario" });
+  }
+};
+export { createUsuario, getUsuario, listUsuario, deleteUsuario, updateUsuario };
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,19 +1,20 @@
-import express from 'express';
-import { logoutUsuario } from '../controllers/Usuario/authUsuarioController';
+import express from "express";
+import { logoutUsuario } from "../controllers/Usuario/authUsuarioController";
 import {
   listUsuario,
   getUsuario,
   createUsuario,
   deleteUsuario,
-} from '../controllers/Usuario/userController';
+} from "../controllers/Usuario/userController";
 
 const router = express.Router();
-router.get('/todosUsuarios', listUsuario);
+router.get("/todosUsuarios", listUsuario);
 
-router.get('/:id', getUsuario);
+router.get("/:id", getUsuario);
 
-router.post('/registrarUsuario', createUsuario);
-router.post('/logoutUsuario', logoutUsuario);
-router.delete('/deletaUsuario/:usuarioId', deleteUsuario);
+router.post("/registrarUsuario", createUsuario);
+router.post("/logoutUsuario", logoutUsuario);
+router.delete("/deletaUsuario/:usuarioId", deleteUsuario);
+router.patch("atualizarUsuario/:usuarioId");
 
 export default router;
